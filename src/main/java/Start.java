dcfec59ad0ea5a955cdf7a7044916a93c4f8a98b
Stefan Schlaghuber <stefan.schlaghuber@gmail.com>
2020-12-23 11:15:53 +0100

    added maven framework support

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -3,7 +3,6 @@ import javax.swing.*;
 public class Start {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-            @Override
             public void run() {
                 MainFrame f = new MainFrame();
                 f.setVisible(true);
